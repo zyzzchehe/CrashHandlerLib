ae2e42ce31877e235573f4eee53adf687242c179
yazhou <yazhou_android@163.com>
2019-12-17 16:31:27 +0800

    Initial commit

---
+++ crashhandlerlib/src/main/java/com/example/crashhandlerlib/CrashHandler.java
@@ -0,0 +1,192 @@
+package com.example.crashhandlerlib;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Build;
+import android.os.Environment;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.lang.Thread.UncaughtExceptionHandler;
+import java.lang.reflect.Field;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class CrashHandler implements UncaughtExceptionHandler {
+
+	public static final String TAG = "CrashHandler";
+
+	private UncaughtExceptionHandler mDefaultHandler;
+	private static CrashHandler INSTANCE = null;
+	private Context mContext;
+	private Map<String, String> infos = new HashMap<String, String>();
+	// 用于格式化日期,作为日志文件名的一部分
+	private DateFormat fileFormatter = new SimpleDateFormat("yy-MM-dd", Locale.getDefault());
+	private DateFormat timeFormatter = new SimpleDateFormat("yy-MM-dd HH:mm:ss:SSS", Locale.getDefault());
+
+	/** 保证只有一个CrashHandler实例 */
+	private CrashHandler() {
+
+	}
+
+	/** 获取CrashHandler实例 ,单例模式 */
+	public static CrashHandler getInstance() {
+		if(INSTANCE == null){
+			INSTANCE = new CrashHandler();
+		}
+		return INSTANCE;
+	}
+
+	/**
+	 * 初始化
+	 * 
+	 * @param context
+	 */
+	public void init(Context context) {
+		mContext = context;
+		// 获取系统默认的UncaughtException处理器
+		mDefaultHandler = Thread.getDefaultUncaughtExceptionHandler();
+		// 设置该CrashHandler为程序的默认处理器
+		Thread.setDefaultUncaughtExceptionHandler(INSTANCE);
+	}
+
+	/**
+	 * 当UncaughtException发生时会转入该函数来处理
+	 */
+	@Override
+	public void uncaughtException(Thread thread, Throwable ex) {
+		if (!handleException(ex) && mDefaultHandler != null) {
+			// 如果用户没有处理则让系统默认的异常处理器来处理
+			mDefaultHandler.uncaughtException(thread, ex);
+		} else {
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				Log.e(TAG, "error : ", e);
+			}
+			// 退出程序
+			android.os.Process.killProcess(android.os.Process.myPid());
+			System.exit(1);
+		}
+	}
+
+	/**
+	 * 自定义错误处理,收集错误信息 发送错误报告等操作均在此完成.
+	 * 
+	 * @param ex
+	 * @return true:如果处理了该异常信息;否则返回false.
+	 */
+	private boolean handleException(Throwable ex) {
+		if (ex == null) {
+			return false;
+		}
+		ex.printStackTrace();//add by yazhou
+		// 使用Toast来显示异常信息
+		new Thread() {
+			@Override
+			public void run() {
+				Looper.prepare();
+				Toast.makeText(mContext, "很抱歉,程序出现异常,即将退出.", Toast.LENGTH_LONG).show();
+				Looper.loop();
+			}
+		}.start();
+		collectDeviceInfo(mContext);
+		saveCrashInfo2File(ex);
+		return true;
+	}
+
+	/**
+	 * 收集设备参数信息
+	 * 
+	 * @param ctx
+	 */
+	public void collectDeviceInfo(Context ctx) {
+		try {
+			PackageManager pm = ctx.getPackageManager();
+			PackageInfo pi = pm.getPackageInfo(ctx.getPackageName(), PackageManager.GET_ACTIVITIES);
+			if (pi != null) {
+				String versionName = pi.versionName == null ? "null" : pi.versionName;
+				String versionCode = pi.versionCode + "";
+				infos.put("versionName", versionName);
+				infos.put("versionCode", versionCode);
+			}
+		} catch (NameNotFoundException e) {
+			Log.e(TAG, "an error occured when collect package info", e);
+		}
+		Field[] fields = Build.class.getDeclaredFields();
+		for (Field field : fields) {
+			try {
+				field.setAccessible(true);
+				infos.put(field.getName(), field.get(null).toString());
+				Log.d(TAG, field.getName() + " : " + field.get(null));
+			} catch (Exception e) {
+				Log.e(TAG, "an error occured when collect crash info", e);
+			}
+		}
+	}
+
+	/**
+	 * 保存错误信息到文件中
+	 * 
+	 * @param ex
+	 * @return 返回文件名称,便于将文件传送到服务器
+	 */
+	private String saveCrashInfo2File(Throwable ex) {
+		StringBuffer sb = new StringBuffer();
+		for (Map.Entry<String, String> entry : infos.entrySet()) {
+			String key = entry.getKey();
+			String value;
+			if (key.equals("TIME")) {
+				value = timeFormatter.format(new Date());
+			} else {
+				value = entry.getValue();
+			}
+			sb.append(key + "=" + value + "\n");
+		}
+
+		Writer writer = new StringWriter();
+		PrintWriter printWriter = new PrintWriter(writer);
+		ex.printStackTrace(printWriter);
+		AtomicReference<Throwable> cause = new AtomicReference<>(ex.getCause());
+		while (cause.get() != null) {
+			ex.getCause().printStackTrace(printWriter);
+		}
+		printWriter.close();
+		String result = writer.toString();
+		sb.append(result);
+
+		try {
+			String time = fileFormatter.format(new Date());
+			String fileName = "crash-" + time + ".log";
+			if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+				String path = Environment.getExternalStorageDirectory().getAbsolutePath()+"/qg_crash/";
+				File dir = new File(path);
+				if (!dir.exists()) {
+					dir.mkdirs();
+				}
+				FileOutputStream fos = new FileOutputStream(path + fileName, true);
+				fos.write(sb.toString().getBytes());
+				fos.close();
+			}
+			return fileName;
+		} catch (Exception e) {
+			Log.e(TAG, "an error occured while writing file...", e);
+		}
+
+		return null;
+	}
+
+}
